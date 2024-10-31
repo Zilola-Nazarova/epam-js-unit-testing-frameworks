ed797ce177cf5b287ae491e12807121ebef89900
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2024-10-31 06:55:53 +0500

    Adjust wdio.conf.js with capabilites to execute tests in Firefox along with Chrome

--- webdriverIO/wdio.conf.js
+++ webdriverIO/wdio.conf.js
@@ -52,6 +52,10 @@ exports.config = {
     capabilities: [{
         // capabilities for local browser web tests
         browserName: 'chrome' // or "firefox", "microsoftedge", "safari"
+    },
+    {
+        // capabilities for local browser web tests
+        browserName: 'firefox' // or "firefox", "microsoftedge", "safari"
     }],
 
     //
@@ -101,7 +105,7 @@ exports.config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    // services: [],
+    services: ['geckodriver'],
     //
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
