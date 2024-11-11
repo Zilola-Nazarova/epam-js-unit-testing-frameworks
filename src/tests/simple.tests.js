182340f7eeba31077ab89a7fa08255e9cbefa729
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2024-11-11 12:11:55 +0500

    Write first test scenario with 4 test cases

--- src/tests/simple.tests.js
+++ src/tests/simple.tests.js
@@ -7,4 +7,36 @@ describe('Doctors page', () => {
     await $("[routerlink='/doctors']").click();
     await expect(browser).toHaveTitle('Appointment Planner - Syncfusion Angular Components Showcase App');
   });
+
+  it('Open modal window for adding a new doctor', async () => {
+    await $("[routerlink='/doctors']").click();
+    await $('.specialization-types button.e-control').click();
+    await expect($('.new-doctor-dialog')).toBeDisplayed();
+  });
+
+  it('Open modal window for adding a new doctor', async () => {
+    await $("[routerlink='/doctors']").click();
+    await $('.specialization-types button.e-control').click();
+    await $('.new-doctor-dialog').waitForDisplayed();
+
+    await $("[name='Name']").setValue('John Doe');
+    await $('#DoctorMobile').setValue('1111111111');
+    await $("[name='Email']").setValue('test@test.com');
+    await $("[name='Education']").setValue('Basic');
+    await $("[name='Designation']").setValue('Test');
+
+    await $('.e-footer-content button.e-primary').click();
+
+    await expect($('#Specialist_8 .name')).toHaveText('Dr. John Doe');
+    await expect($('#Specialist_8 .education')).toHaveText('Basic', { ignoreCase: true });
+  });
+
+  it('Open modal window for adding a new doctor', async () => {
+    await $("[routerlink='/doctors']").click();
+    await $('.specialization-types button.e-control').click();
+    await $('.new-doctor-dialog').waitForDisplayed();
+
+    await $('.new-doctor-dialog .e-dlg-closeicon-btn').click();
+    await expect($('.new-doctor-dialog')).not.toBeDisplayed();
+  });
 });
