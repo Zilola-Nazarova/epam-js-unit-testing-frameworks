b2e7e0913ec463c7ec80ffeb2879a196a3380139
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2024-11-12 15:21:51 +0500

    Create a BasePage in order to not repeat open method in all pages

--- src/po/pages/dashboard.page.js
+++ src/po/pages/dashboard.page.js
@@ -1,13 +1,15 @@
+import BasePage from "./base.page";
 import SideMenuComponent from "../components/common/sidemenu.component";
 
-class DashboardPage {
+class DashboardPage extends BasePage {
   constructor() {
+    super('https://ej2.syncfusion.com/showcase/angular/appointmentplanner/#/dashboard');
     this.sideMenu = new SideMenuComponent();
   }
 
-  async open() {
-    await browser.url('https://ej2.syncfusion.com/showcase/angular/appointmentplanner/#/dashboard');
-  }
+  // async open() {
+  //   await browser.url('https://ej2.syncfusion.com/showcase/angular/appointmentplanner/#/dashboard');
+  // }
 };
 
 export default DashboardPage;
--- src/po/pages/doctors.page.js
+++ src/po/pages/doctors.page.js
@@ -1,15 +1,18 @@
+import BasePage from "./base.page";
+
 import ListHeaderComponent from "../components/doctors/list-header.component";
 import AddDoctorComponent from "../components/doctors/add-doctor.component";
 
-class DoctorsPage {
+class DoctorsPage extends BasePage {
   constructor() {
+    super('https://ej2.syncfusion.com/showcase/angular/appointmentplanner/#/doctors');
     this.listHeaderComponent = new ListHeaderComponent();
     this.addDoctorModal = new AddDoctorComponent();
   }
 
-  async open() {
-    return browser.open('https://ej2.syncfusion.com/showcase/angular/appointmentplanner/#/doctors');
-  }
+  // async open() {
+  //   return browser.url('https://ej2.syncfusion.com/showcase/angular/appointmentplanner/#/doctors');
+  // }
 };
 
 export default DoctorsPage;
