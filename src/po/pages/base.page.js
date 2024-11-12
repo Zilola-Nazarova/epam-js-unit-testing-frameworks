13acbead54ad6ef64b8c39166d922a553e3d8519
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2024-11-12 16:06:47 +0500

    Create SideMenu's isntance in BasePage to not repeat it in other pages

--- src/po/pages/base.page.js
+++ src/po/pages/base.page.js
@@ -1,9 +1,11 @@
 import Header from "../components/common/header.component";
+import SideMenu from "../components/common/sidemenu.component";
 
 class BasePage {
   constructor(url) {
     this.url = url;
     this.header = new Header();
+    this.sideMenu = new SideMenu();
   }
 
   async open() {
--- src/po/pages/dashboard.page.js
+++ src/po/pages/dashboard.page.js
@@ -1,10 +1,8 @@
 import BasePage from "./base.page";
-import SideMenuComponent from "../components/common/sidemenu.component";
 
 class DashboardPage extends BasePage {
   constructor() {
     super('/showcase/angular/appointmentplanner/#/dashboard');
-    this.sideMenu = new SideMenuComponent();
   }
 };
 
